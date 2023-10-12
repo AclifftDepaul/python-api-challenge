7cc4df7a7d2d0305604913518c58302a263654ff
AclifftDepaul <clifftandrew@gmail.com>
2023-10-12 15:55:07 -0500

    finished part one weather py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "2448256946a08e880f30baf1974390ba"
+weather_api_key = "0fdb11ef859b9a26c3c5595cb03525fa"
 
 # Geoapify API Key
 geoapify_key = "YOUR KEY HERE"
